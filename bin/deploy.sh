9a528138f8f6118c060317f3c725d1c08080ab29
Elliott deLaunay <edelauna@gmail.com>
2023-04-05 20:43:35 +0000

    feat(deploy): env var for env file

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker stop discord-bot-ai
 docker rm discord-bot-ai
-docker run --name discord-bot-ai -d --env-file .env -v /app/logs:/app/logs "${DOCKER_IMAGE}"
-
+docker run --name discord-bot-ai -d --env-file "${ENV_FILE}" -v /app/logs:/app/logs "${DOCKER_IMAGE}"
