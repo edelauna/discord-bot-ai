8a63b960c1d731f912ee707257993f6bee2e78c3
Elliott deLaunay <edelauna@gmail.com>
2023-04-05 02:44:47 +0000

    fix(message): deleting old responses, after recorded

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,18 @@
 import { token } from './config/app';
 import { client } from './config/client';
 import { registerEventHandlers } from './config/events';
+import { logger } from './util/log';
 
 registerEventHandlers(client);
 
 client.login(token);
+
+process.on('uncaughtException', (err) => {
+    logger.error(`Uncaught Exception: ${err.message}`, err);
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    logger.error(`Unhandled Rejection at: ${promise}, reason: ${reason}`);
+    process.exit(1);
+});
