a7684ce4bee0ef41f1be0ad63d1fcc92b0949587
Elliott deLaunay <edelauna@gmail.com>
2023-04-05 20:23:05 +0000

    fix(workflow): updating scripts and fixing deploy logic

--- bin/package.sh
+++ bin/package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker rmi $(docker images -f dangling=true -q)
-docker rmi $(docker images -q ${DOCKER_IMAGE})
-docker pull ${DOCKER_IMAGE}:latest
+docker rmi "$(docker images -f dangling=true -q)"
+docker rmi "$(docker images -q "${DOCKER_IMAGE}")"
+docker pull "${DOCKER_IMAGE}":latest
