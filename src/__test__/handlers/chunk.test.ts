3b79763ea88bf96a08767c5f8755cea7df86163d
Elliott deLaunay <edelauna@gmail.com>
2023-04-07 19:57:35 +0000

    refact(chunk): simplifying chunk handling logic

--- src/__test__/handlers/chunk.test.ts
+++ src/__test__/handlers/chunk.test.ts
@@ -1,7 +1,6 @@
 import { DISCORD_MAX_CHARS } from '../../config/app';
-import { ChunkHandlerError } from '../../errors/chunk';
 import { chunkHandler, clearActiveChunks } from '../../handlers/chunk';
-import { send, sendTyping } from '../../util/send';
+import { send } from '../../util/send';
 
 jest.mock('../../util/send', () => ({
     send: jest.fn(),
@@ -13,66 +12,45 @@ describe('chunkHandler', () => {
     const data = { content: 'Hello, world!' };
 
     let sendMock: jest.MockedFunction<typeof send>;
-    let sendTypingMock: jest.MockedFunction<typeof sendTyping>;
 
     beforeEach(() => {
-        jest.resetAllMocks();
         sendMock = send as jest.MockedFunction<typeof send>;
-        /**
-         * I don't think this is the correct way to test this - probably need to look at how to handle the
-         * stream of data from /handlers/openai/chat-completions to process all the promises in order.
-         * maybe like a Promise queue which awaits each promise in sequence.
-         */
-        sendMock.mockImplementation(async () => await sendTypingMock(channelId));
-        sendTypingMock = sendTyping as jest.MockedFunction<typeof sendTyping>;
-        sendTypingMock.mockResolvedValue(undefined);
+        sendMock.mockResolvedValue();
         // Clear activeChunks before each test
         clearActiveChunks();
     });
 
     afterEach(() => {
-        sendMock.mockRestore();
+        jest.clearAllMocks();
     });
 
     it('should handle a single chunk', () => {
-        const payload = { channelId, end: false, data };
-        const result = chunkHandler(payload);
-        expect(result).toBe(data.content);
+        const payload = { channelId, data };
+        chunkHandler(payload);
+        chunkHandler({ channelId: channelId, last: true });
+        expect(sendMock).toHaveBeenCalledWith(channelId, data.content);
     });
 
     it('should handle multiple chunks', () => {
-        const payload1 = { channelId, end: false, data: { role: 'assistant' } };
+        const payload1 = { channelId, data: { role: 'assistant' } };
         const longString = 'a'.repeat(2 * DISCORD_MAX_CHARS);
-        const payload2 = { channelId, end: false, data: { content: longString } };
+        const payload2 = { channelId, data: { content: longString } };
         chunkHandler(payload1);
-        const result = chunkHandler(payload2);
-        expect(result).toBe('a'.repeat(493));
-        expect(sendMock).toHaveBeenCalledTimes(1);
-        expect(sendTypingMock).toHaveBeenCalledTimes(1);
+        chunkHandler(payload2);
+        expect(sendMock).toHaveBeenCalledWith(channelId, 'a'.repeat(493));
     });
 
     it('should handle a mix of long and short chunks', () => {
         const shortString = 'Hello';
         const longString = 'a'.repeat(2 * DISCORD_MAX_CHARS);
-        const payload1 = { channelId, end: false, data: { content: shortString } };
-        const payload2 = { channelId, end: false, data: { content: longString } };
-        const payload3 = { channelId, end: true };
-        const result1 = chunkHandler(payload1);
-        expect(result1).toBe(payload1.data.content);
-        const result2 = chunkHandler(payload2);
-        expect(result2).toBe('a'.repeat(5));
-        const result3 = chunkHandler(payload3);
-        expect(result3).toBe('a'.repeat(5));
-        expect(sendMock).toHaveBeenCalledTimes(2);
-        expect(sendTypingMock).toHaveBeenCalledTimes(2);
-    });
-
-    it('should throw an error if an error is passed', () => {
-        const error = new Error('Test error');
-        const errorHandler = jest.fn();
-        const expectedError = new ChunkHandlerError('Error while building chunk', error);
-        const payload = { channelId, resolver: { error: errorHandler }, error };
-        chunkHandler(payload);
-        expect(errorHandler).toHaveBeenCalledWith(expectedError);
+        const payload1 = { channelId, data: { content: shortString } };
+        const payload2 = { channelId, data: { content: longString } };
+        const payload3 = { channelId, last: true };
+        chunkHandler(payload1);
+        chunkHandler(payload2);
+        chunkHandler(payload3);
+        expect(sendMock).toHaveBeenCalledWith(channelId, payload1.data.content + 'a'.repeat(488));
+        expect(sendMock).toHaveBeenCalledWith(channelId, 'a'.repeat(493));
+        expect(sendMock).toHaveBeenCalledWith(channelId, 'a'.repeat(5));
     });
 });
--- src/errors/chunk.ts
+++
@@ -1,9 +0,0 @@
-class ChunkHandlerError extends Error {
-    constructor(message: string, error?: Error) {
-        super(message);
-        this.name = 'ChunkHandlerError';
-        this.stack = error?.stack || this.stack;
-    }
-}
-
-export { ChunkHandlerError };
