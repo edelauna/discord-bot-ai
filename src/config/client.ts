7949dde2630820c18bd33bbbcd83d23685a2c51b
Elliott deLaunay <edelauna@gmail.com>
2023-04-07 20:10:07 +0000

    feat(client): cleaning up cache config

--- src/config/client.ts
+++ src/config/client.ts
@@ -1,4 +1,4 @@
-import { Client, Collection, GatewayIntentBits, Options } from 'discord.js';
+import { Client, Collection, GatewayIntentBits } from 'discord.js';
 import type { CommandInteraction } from 'discord.js';
 
 interface SlashCommand {
@@ -12,44 +12,6 @@ interface AppClient extends Client {
 }
 // Create a new client instance
 const client = new Client({
-    makeCache: Options.cacheWithLimits({
-        // guild.commands
-        ApplicationCommandManager: 0,
-        // guild.emojis
-        BaseGuildEmojiManager: 0,
-        // client.channels
-        // ChannelManager: 0,
-        // guild.channels
-        // GuildChannelManager: 0,
-        // guild.bans
-        GuildBanManager: 0,
-        // guild.invites
-        GuildInviteManager: 0,
-        // guild.members
-        GuildMemberManager: 0,
-        // guild.stickers
-        GuildStickerManager: 0,
-        // guild.scheduledEvents
-        GuildScheduledEventManager: 0,
-        // channel.messages
-        MessageManager: 0,
-        // guild.presences
-        PresenceManager: 0,
-        // message.reactions
-        ReactionManager: 0,
-        // reaction.users
-        ReactionUserManager: 0,
-        // guild.stageInstances
-        StageInstanceManager: 0,
-        // channel.threads
-        ThreadManager: 0,
-        // threadchannel.members
-        ThreadMemberManager: 0,
-        // client.users
-        UserManager: 0,
-        // guild.voiceStates
-        VoiceStateManager: 0,
-    }),
     intents: [
         GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent,
     ],
