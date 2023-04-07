a9e7f178174cd6b28eb1b1eed702999ecdd955a3
Elliott deLaunay <edelauna@gmail.com>
2023-04-07 19:59:49 +0000

    feat(files): adding file handling logic

---
+++ src/handlers/files.ts
@@ -0,0 +1,52 @@
+import type { Snowflake } from 'discord.js';
+import { setTimeout } from 'timers/promises';
+
+interface File {
+    isText: boolean | null,
+    content?: string,
+    downloadPromise?: Promise<void>
+}
+const files: Record<Snowflake, File> = {};
+
+const downloadTextFile = async (attachmentId: Snowflake, url: string) => {
+    const response = await fetch(url);
+    if (!response.body) { return; }
+
+    const reader = response.body.getReader();
+    let partial = '';
+    let isText = true;
+    while (isText) {
+        const { value, done } = await reader.read();
+        if (done) { break; }
+        const decoder = new TextDecoder('ascii');
+        const temp = decoder.decode(value.buffer);
+        // There could be control characters within a valid text file.
+        // eslint-disable-next-line no-control-regex
+        isText = /^[\x00-\x7F]*$/.test(temp);
+        files[attachmentId] = { isText };
+        partial = isText ? partial + temp : partial;
+    }
+    files[attachmentId] = { isText, content: partial };
+};
+
+const addFile = (attachmentId: Snowflake, url: string) =>
+    files[attachmentId] = { isText: null, downloadPromise: downloadTextFile(attachmentId, url) };
+
+const isTextFile = async (attachmentId: Snowflake, url: string, ttl = 6): Promise<boolean> => {
+    if (!files[attachmentId]) { addFile(attachmentId, url); }
+    if (files[attachmentId].isText !== null) { return files[attachmentId].isText as boolean; }
+    let isText = null;
+    while (isText == null && ttl > 0) {
+        isText = await setTimeout(500, files[attachmentId]?.isText);
+        ttl--;
+    }
+    return isText || false;
+};
+
+const getFileContent = async (attachmentId: Snowflake) => {
+    if (!files[attachmentId]) { return ''; }
+    await files[attachmentId].downloadPromise;
+    return files[attachmentId].content;
+};
+
+export { isTextFile, getFileContent, files };
