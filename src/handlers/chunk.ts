a5e343210eb81b88cb8193593c9d44f59992ddd0
Elliott deLaunay <edelauna@gmail.com>
2023-04-07 19:57:35 +0000

    refact(chunk): simplifying chunk handling logic

--- src/handlers/chunk.ts
+++ src/handlers/chunk.ts
@@ -1,45 +1,29 @@
 import type { Snowflake } from 'discord.js';
 import { DISCORD_MAX_CHARS } from '../config/app';
-import { ChunkHandlerError } from '../errors/chunk';
 import { safeChunk } from '../util/chunk';
 import { send, sendTyping } from '../util/send';
 import { messageHandler } from './openai/message';
 import { endMessage } from '../util/openai/messages';
 
-interface Resolver {
-    end?: (value: void) => void;
-    error?: (reason?: unknown) => void;
-}
-
 interface ChunkHandler {
     // want to limit interactions to one channel at a time
     channelId: Snowflake,
-    resolver?: Resolver,
+    last?: boolean,
     data?: {
         role?: string
         content?: string
-    },
-    error?: Error
+    }
 }
 
 const activeChunks = new Map<Snowflake, string>();
-/**
- * Pre-req: end == true, must be sent after the last data event.
- *
- * Receives incremental data, checks if we're over the character count,
- * if so - creates an array of safe chunks - sends all but the last item of the array.
- * On end - sends whatever is left - clears cache
- * @param ChunkHandle - attributes used for transforming messages from openai to discord format
- */
-const chunkHandler = ({ channelId, resolver, data, error }: ChunkHandler) => {
-    if (resolver) return handleLastChunk(channelId, resolver, error);
+
+const chunkHandler = async ({ channelId, data, last }: ChunkHandler) => {
+    if (last) return handleLastChunk(channelId);
 
     const newChunk = data?.content || '';
     const draftChunk = !activeChunks.has(channelId) ? newChunk : activeChunks.get(channelId) + newChunk;
     const finalChunk = handleChunk(channelId, draftChunk);
     activeChunks.set(channelId, finalChunk);
-
-    return finalChunk;
 };
 
 const handleChunk = (channelId: Snowflake, chunk: string) => {
@@ -49,26 +33,19 @@ const handleChunk = (channelId: Snowflake, chunk: string) => {
         const content = chunks.shift() as string;
         // need to callback openai messageHandler to eventually store the response
         messageHandler({ channelId, chunk: content });
+        // important that this is a non blocking call
         send(channelId, content).then(() => sendTyping(channelId));
     }
     return chunks[0];
 };
 
-const handleLastChunk = (channelId: Snowflake, resolver: Resolver, error?: Error) => {
+const handleLastChunk = async (channelId: Snowflake) => {
     const lastChunk = activeChunks.get(channelId) || '';
     activeChunks.delete(channelId);
     messageHandler({ channelId, chunk: lastChunk, end: true });
-    if (resolver.error) resolver.error(new ChunkHandlerError('Error while building chunk', error));
-    send(channelId, lastChunk).then(() => {
-        sendTyping(channelId).then(() => {
-            send(channelId, endMessage()).then(() => {
-                if (resolver.end) return resolver.end();
-                if (!resolver.error) throw new ChunkHandlerError('No Resolver.end passed to handleLastChunk');
-            });
-        });
-    });
-
-    return lastChunk || '';
+    await send(channelId, lastChunk);
+    await sendTyping(channelId);
+    await send(channelId, endMessage());
 };
 
 const clearActiveChunks = () => activeChunks.clear();
