9a415a44b3868c80a679fd5daaa23a1376ccaaf3
Elliott deLaunay <edelauna@gmail.com>
2023-04-07 20:10:42 +0000

    refact(message): removing log of every message

--- src/config/events/message.ts
+++ src/config/events/message.ts
@@ -2,12 +2,10 @@ import { Events } from 'discord.js';
 import type { Message } from 'discord.js';
 import { client } from '../client';
 import { interactWithOpenAi } from '../../svcs/runner';
-import { logger } from '../../util/log';
 
 module.exports = {
     name: Events.MessageCreate,
     async execute(message: Message) {
-        logger.info(`New message in channel ${message.channel}: ${message.content}`);
         if (message.author.id !== client.user?.id) return interactWithOpenAi(message);
     },
 };
