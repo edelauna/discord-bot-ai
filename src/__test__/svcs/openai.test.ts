c8baa2d35b476b496524ff0ff79c1bac28e40468
Elliott deLaunay <edelauna@gmail.com>
2023-04-10 15:04:39 +0000

    feat(openai): handling interrupts - adding logging for what seems like really long response times form openai

--- src/__test__/svcs/openai.test.ts
+++ src/__test__/svcs/openai.test.ts
@@ -2,10 +2,14 @@ import { AxiosResponse } from 'axios';
 import { openai } from '../../config/openai';
 import { completionMessage } from '../../svcs/openai';
 import { streamHandler } from '../../handlers/openai/stream';
+import { runners } from '../../svcs/runner';
+import type { Message } from 'discord.js';
 
 jest.mock('../../handlers/openai/stream');
 
 describe('completionMessage', () => {
+    afterEach(() => jest.resetAllMocks());
+
     test('calls the provided callback with chat completions', async () => {
         const mockStream = jest.fn();
         const mockResponse = {
@@ -15,8 +19,9 @@ describe('completionMessage', () => {
             mockResponse as unknown as AxiosResponse,
         );
         const mockStreamHandler = streamHandler as jest.MockedFunction<typeof streamHandler>;
-
-        await completionMessage('channel1');
+        const mockRunners = runners as jest.MockedObject<typeof runners>;
+        mockRunners['ref-123'] = { status: 'running', message: {} as Message };
+        await completionMessage('ref-123');
 
         expect(mockCreateChatCompletion).toHaveBeenCalledWith({
             model: 'gpt-3.5-turbo',
@@ -28,6 +33,31 @@ describe('completionMessage', () => {
         }, {
             responseType: 'stream',
         });
-        expect(mockStreamHandler).toHaveBeenCalledWith(mockStream, 'channel1');
+        expect(mockStreamHandler).toHaveBeenCalledWith(mockStream, 'ref-123');
+    });
+    test('does not call the provided callback when status aborted', async () => {
+        const mockStream = jest.fn();
+        const mockResponse = {
+            data: mockStream,
+        };
+        const mockCreateChatCompletion = jest.spyOn(openai, 'createChatCompletion').mockResolvedValue(
+            mockResponse as unknown as AxiosResponse,
+        );
+        const mockStreamHandler = streamHandler as jest.MockedFunction<typeof streamHandler>;
+        const mockRunners = runners as jest.MockedObject<typeof runners>;
+        mockRunners['ref-123'] = { status: 'aborted', message: {} as Message };
+        await completionMessage('ref-123');
+
+        expect(mockCreateChatCompletion).toHaveBeenCalledWith({
+            model: 'gpt-3.5-turbo',
+            messages: [{
+                'content': 'You are a helpful assistant that responds using markdown.',
+                'role': 'system',
+            }],
+            stream: true,
+        }, {
+            responseType: 'stream',
+        });
+        expect(mockStreamHandler).toHaveBeenCalledTimes(0);
     });
 });
--- src/svcs/openai.ts
+++ src/svcs/openai.ts
@@ -2,18 +2,23 @@ import { IncomingMessage } from 'node:http';
 import { openai } from '../config/openai';
 import { messages } from '../util/openai/messages';
 import { streamHandler } from '../handlers/openai/stream';
-import { Snowflake } from 'discord.js';
+import { ReferenceId, runners } from './runner';
+import { logger } from '../util/log';
 
 
-const completionMessage = async (channelId: Snowflake) => {
+const completionMessage = async (referenceId: ReferenceId) => {
+    const startTime = Date.now();
     const response = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
         messages: messages,
         stream: true,
     }, { responseType: 'stream' });
-
+    const responseTime = Date.now() - startTime;
+    // This all of a sudden became really long... ~10s
+    logger.info(`openai.createChatCompletion time until response.data stream:${((responseTime % 60000) / 1000).toFixed(1)}s, ${responseTime % 1000}ms`);
+    if (runners[referenceId].status == 'aborted') { return; }
     const stream = response.data as unknown as IncomingMessage;
-    await streamHandler(stream, channelId);
+    await streamHandler(stream, referenceId);
 };
 
 export { completionMessage };
