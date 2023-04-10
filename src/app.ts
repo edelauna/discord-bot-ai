6e8a60be1f749c27c67d2fc9fcb6061564ed6dcd
Elliott deLaunay <edelauna@gmail.com>
2023-04-10 14:58:08 +0000

    feat(app): improving unhandled logs

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,12 @@ import { logger } from './util/log';
 
 const app = async () => {
     process.on('uncaughtException', (err) => {
-        logger.error(`Uncaught Exception: ${err.message}`, err);
+        logger.error(`Uncaught Exception: ${err.message}`, { stack: err.stack });
         process.exit(1);
     });
 
     process.on('unhandledRejection', (reason, promise) => {
-        logger.error(`Unhandled Rejection at: ${promise}, reason: ${reason}`);
+        logger.error(`Unhandled Rejection at: ${promise}, reason: ${reason}`, { promise });
         process.exit(1);
     });
 
