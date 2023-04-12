3487e387e4d4020573f359d29906931d941fd235
Elliott deLaunay <edelauna@gmail.com>
2023-04-12 01:22:18 +0000

    feat(knex): adding environment variable for db, along with new migration to support custom prompts per channel

--- knexfile.ts
+++ knexfile.ts
@@ -1,7 +1,8 @@
 import type { Knex } from 'knex';
 import { config as appDbConfig } from './src/config/db';
+import { environment } from './src/config/app';
 
-const config: Knex.Config = appDbConfig(process.env.NODE_ENV);
+const config: Knex.Config = appDbConfig(environment);
 /**
  * This is needed for running knex from cli or via npm scripts
  */
