d785acf9f0139dd248a77bbf1a7f0dba15c22cac
Elliott deLaunay <edelauna@gmail.com>
2023-04-14 01:24:25 +0000

    feat(reset-messages): no-op is channel is not active

--- src/handlers/commands/reset-messages.ts
+++ src/handlers/commands/reset-messages.ts
@@ -1,7 +1,12 @@
 import type { CommandInteraction } from 'discord.js';
 import { resetMessages } from '../../util/openai/messages';
+import { getChannel } from '../../config/db/channels';
 
 const execute = async (interaction: CommandInteraction) => {
+    const channel = await getChannel(interaction.channelId);
+    if (!channel || !channel.active) {
+        return await interaction.reply('-bot-ai not enabled in channel. See **/channel** command.');
+    }
     resetMessages(interaction.channelId);
     await interaction.reply('Chat history reset.');
 };
