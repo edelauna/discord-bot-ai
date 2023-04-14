3e639d9dfe202d1c45cafc283396781fa231a7b5
Elliott deLaunay <edelauna@gmail.com>
2023-04-14 01:24:25 +0000

    feat(reset-messages): no-op is channel is not active

--- src/__test__/handlers/commands/reset-messages.test.ts
+++ src/__test__/handlers/commands/reset-messages.test.ts
@@ -1,8 +1,10 @@
 import { CommandInteraction } from 'discord.js';
 import { resetMessages } from '../../../util/openai/messages';
 import { execute } from '../../../handlers/commands/reset-messages';
+import { getChannel } from '../../../config/db/channels';
 
 jest.mock('../../../util/openai/messages');
+jest.mock('../../../config/db/channels');
 
 describe('execute function', () => {
     test('should call resetMessages function and reply to interaction', async () => {
@@ -10,10 +12,20 @@ describe('execute function', () => {
         const resetMessagesMock = resetMessages as jest.MockedFunction<typeof resetMessages>;
         interaction.channelId = '12345';
         interaction.reply = jest.fn();
+        (getChannel as jest.Mock).mockReturnValueOnce({ active: true, channel_id: '123' });
 
         await execute(interaction);
 
         expect(resetMessagesMock).toHaveBeenCalledWith('12345');
         expect(interaction.reply).toHaveBeenCalledWith('Chat history reset.');
+    });
+    test('should noop if channel is not found', async () => {
+        const interaction = {} as CommandInteraction;
+        interaction.channelId = '12345';
+        interaction.reply = jest.fn();
+
+        await execute(interaction);
+
+        expect(interaction.reply).toHaveBeenCalledWith('-bot-ai not enabled in channel. See **/channel** command.');
     });
 });
