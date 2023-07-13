813b42f9ed4efbb25ed8529c16c9adaa05fefbef
Elliott deLaunay <edelauna@gmail.com>
2023-07-13 02:28:30 +0000

    feat(log): rolling logs, logging errors in own file

--- src/util/log.ts
+++ src/util/log.ts
@@ -5,7 +5,19 @@ const logger = winston.createLogger({
     format: winston.format.json(),
     defaultMeta: { service: 'discord-bot-ai' },
     transports: [
-        new winston.transports.File({ filename: 'logs/development.log' }),
+        new winston.transports.File({
+            filename: 'logs/error.log',
+            level: 'error',
+            maxsize: 5242880,
+            maxFiles: 3,
+            tailable: true,
+        }),
+        new winston.transports.File({
+            filename: 'logs/development.log',
+            maxsize: 5242880,
+            maxFiles: 1,
+            tailable: true,
+        }),
         new winston.transports.Console({ format: winston.format.simple() }),
     ],
 });
