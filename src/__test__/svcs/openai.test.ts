7bd1b611f416c149009514189b4f9a5cabffd6e9
Elliott deLaunay <edelauna@gmail.com>
2023-07-13 01:32:15 +0000

    feat(openai): adding gpt-4 model

--- src/__test__/svcs/openai.test.ts
+++ src/__test__/svcs/openai.test.ts
@@ -32,7 +32,7 @@ describe('completionMessage', () => {
         await completionMessage('ref-123');
 
         expect(mockCreateChatCompletion).toHaveBeenCalledWith({
-            model: 'gpt-3.5-turbo-16k',
+            model: 'gpt-4',
             messages: [{
                 'content': 'You are a helpful assistant that responds using markdown.',
                 'role': 'system',
@@ -62,7 +62,7 @@ describe('completionMessage', () => {
         await completionMessage('ref-123');
 
         expect(mockCreateChatCompletion).toHaveBeenCalledWith({
-            model: 'gpt-3.5-turbo-16k',
+            model: 'gpt-4',
             messages: [{
                 'content': 'You are a helpful assistant that responds using markdown.',
                 'role': 'system',
